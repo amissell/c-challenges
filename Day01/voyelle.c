9c839695d4f3d15d2f6d5ebf7264166596ecb791
Amal Issellay <amalissellay01@gmail.com>
2025-09-23 13:49:34 +0100

    second challenge

---
+++ Day01/voyelle.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+char ft_voyelle(char c)
+{
+    switch (c)
+    {
+        case 'a':
+        printf("voyelle");
+        break;
+        case 'e':
+        printf("voyelle");
+        break;
+        case 'i':
+        printf("voyelle");
+        break;
+        case 'o':
+        printf("voyelle");
+        break;
+        case 'u':
+        printf("voyelle");
+        break;
+        case 'y':
+        printf("voyelle");
+        break;
+    }
+
+}
+
+
+int main()
+{
+    char  x = 'b';
+    ft_voyelle(x);
+    return 0;
+}

b16d6da713568015e51b41d547c753913008f2b2
Amal Issellay <amalissellay01@gmail.com>
2025-09-24 16:15:02 +0100

    add the challenge of multiplication in array

---
+++ Day02/ft_multiple_arr.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int x;
+
+    int factor;
+    printf("put the nember of elements: ");
+    scanf("%d", &x);
+
+    printf("the elments: ");
+    int arr[x];
+    for (int  i = 0; i < x; i++)
+    {
+        printf("element%d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the multiplication factor: ");
+    scanf("%d", &factor);
+
+    for (int i = 0; i < x; i++)
+    {
+        arr[i] *= factor;
+    }
+    for(int i =0; i < x; i++){
+        printf("the new arr: %d", arr[i]);
+
+    }
+
+}
